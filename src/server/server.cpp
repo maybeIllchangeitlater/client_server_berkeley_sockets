588212f9a7d6b9a4b60160497cb613180a050ad5
monke <porozheg@gmail.com>
2024-01-26 22:05:04 +0300

    barebones server, not finished

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -8,13 +8,32 @@ namespace s21{
                 accept_thread_ = std::thread([this](){
                     while (true){
                         try{
-                            Socket new_connection = acceptor_.AsyncAccept().get();
-                            connections_.emplace_back(std::move(new_connection));
+                            connections_.emplace_back(acceptor_.AsyncAccept().get());
+                            std::cout << "new connection" << std::endl;
+                            HandlePing(--connections_.end());
                         } catch (const std::exception &e){
-                            std::cout << e.what();
+                            std::cout << e.what() << std::endl;
                         }
                     }
                 });
+            }
+
+            void Server::HandlePing(connections_iterator connection) {
+                char buffer[1024];
+                while (true) {
+                    if((*connection)->Receive(buffer, sizeof(buffer)) > 0) {
+                        std::string logged(Timestamper::GetTimestamp() + " " + buffer);
+                        msg_que_.EmplaceBack(std::make_pair(connection, std::move(logged)));
+                    } else {
+                        std::cout << "connection closed by client";
+                        connections_.erase(connection); //add mutex
+                        break;
+                    }
+                }
+            }
+            Server::~Server() {
+                accept_thread_.join();
+                log_file_.close();
             }
         }
     }
--- src/server/server.h
+++ src/server/server.h
@@ -1,20 +1,30 @@
 #ifndef CLIENT_SERVER_BERKELEY_SOCKETS_SERVER_SERVER_H
 #define CLIENT_SERVER_BERKELEY_SOCKETS_SERVER_SERVER_H
 
-#include <vector>
+#include <list>
 #include <thread>
+#include <fstream>
 #include "acceptor.h"
 #include "../common/socket.h"
+#include "../common/timestamper.h"
+#include "../common/thread_safe_que.h"
 
 namespace s21 {
     namespace tcp {
         namespace ip {
             class Server {
+            public:
+                using connections_iterator = std::list<std::shared_ptr<Socket>>::iterator;
                 explicit Server(unsigned short port);
+                ~Server();
+            private:
+                void HandlePing(connections_iterator connection);
             private:
                 Acceptor acceptor_;
-                std::vector<Socket> connections_;
+                std::list<std::shared_ptr<Socket>> connections_;
+                s21::ThreadSafeQ<std::pair<connections_iterator, std::string>> msg_que_;
                 std::thread accept_thread_;
+                std::ofstream log_file_;
             };
         } //ip
     } //tcp
