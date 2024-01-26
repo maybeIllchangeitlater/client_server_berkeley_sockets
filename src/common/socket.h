ff7887039a5e20a32e7643cbefc3a3e7cc4b918c
monke <porozheg@gmail.com>
2024-01-26 21:36:34 +0300

    fixes related to shared ptr migration

--- src/common/socket.h
+++ src/common/socket.h
@@ -13,32 +13,32 @@ namespace s21 {
     namespace tcp {
         namespace ip {
         class Socket : public std::enable_shared_from_this<Socket>{
-            public:
-                static std::shared_ptr<Socket> CreateSocket(){
+        public:
+            ~Socket();
+            static std::shared_ptr<Socket> CreateSocket(){
                     struct MakeSharedEnabler : public Socket {};
                     return std::make_shared<MakeSharedEnabler>();
                 }
-                ///binds sockets to address and port
+            ///binds sockets to address and port
                 void Bind(unsigned short port = 0, const char * address = "127.0.0.1");
-                ///Reads into buffer of max length buffer_length. Returns amount of bytes read
+            ///Reads into buffer of max length buffer_length. Returns amount of bytes read
                 size_t Receive(char *buffer, size_t buffer_length);
-                ///Sends data. Returns size of data sent
+            ///Sends data. Returns size of data sent
                 size_t Send(int file_descriptor, const char * data, size_t data_length);
-            protected:
-                static std::shared_ptr<Socket> CreateSocket(int fd){
+        protected:
+            static std::shared_ptr<Socket> CreateSocket(int fd){
                     struct MakeSharedEnabler : public Socket {
                         MakeSharedEnabler(int fd) : Socket(fd) {}
                     };
                     return std::make_shared<MakeSharedEnabler>(fd);
                 }
-                static sockaddr_in InitializeSocketAddress(const char * address, unsigned short port);
-                int fd_;
-            private:
-                ///Creates socket for TCP/IP connection
-                Socket();
-                explicit Socket(int fd);
-                ~Socket();
-                void Close();
+            static sockaddr_in InitializeSocketAddress(const char * address, unsigned short port);
+            ///Creates socket for TCP/IP connection
+            Socket();
+            int fd_;
+            explicit Socket(int fd);
+        private:
+            void Close();
         };
 
         } //ip
