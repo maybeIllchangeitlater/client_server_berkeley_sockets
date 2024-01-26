22fedf59e5c77513ee9305e10e8b3db8cc294764
monke <porozheg@gmail.com>
2024-01-26 21:51:02 +0300

    slight refactor/comments to code

--- src/common/socket.h
+++ src/common/socket.h
@@ -12,8 +12,17 @@
 namespace s21 {
     namespace tcp {
         namespace ip {
-        class Socket : public std::enable_shared_from_this<Socket>{
+        ///non-copyable, non-movable object representing BSD/UNIX internet socket
+        ///for TCP/IPv4 connection
+        ///Accessed from outside by shared_ptr
+        class Socket {
             public:
+                Socket(const Socket &other) = delete;
+                Socket(Socket &&other) = delete;
+                Socket& operator=(const Socket &other) = delete;
+                Socket& operator=(Socket &&other) = delete;
+                ~Socket();
+                ///Creates shared ptr on socket for TCP/IP connection
                 static std::shared_ptr<Socket> CreateSocket(){
                     struct MakeSharedEnabler : public Socket {};
                     return std::make_shared<MakeSharedEnabler>();
@@ -25,19 +34,22 @@ namespace s21 {
                 ///Sends data. Returns size of data sent
                 size_t Send(int file_descriptor, const char * data, size_t data_length);
             protected:
+                ///Creates socket for TCP/IP connection
+                Socket();
+                ///Creates shared ptr on socket for TCP/IP connection based on file descriptor of existing already created
+                ///socket()
                 static std::shared_ptr<Socket> CreateSocket(int fd){
                     struct MakeSharedEnabler : public Socket {
                         MakeSharedEnabler(int fd) : Socket(fd) {}
                     };
                     return std::make_shared<MakeSharedEnabler>(fd);
                 }
+                ///Initialize socket address structure for IPv4 internet protocol
+                ///for further work with Berkeley sockets API
                 static sockaddr_in InitializeSocketAddress(const char * address, unsigned short port);
                 int fd_;
             private:
-                ///Creates socket for TCP/IP connection
-                Socket();
                 explicit Socket(int fd);
-                ~Socket();
                 void Close();
         };
 
--- src/server/acceptor.h
+++ src/server/acceptor.h
@@ -7,20 +7,26 @@
 namespace s21{
     namespace tcp {
         namespace ip {
+            ///Acceptor socket for server. Responsible for accepting connections
             class Acceptor : protected Socket{
             public:
+                constexpr static const short BACKLOG_LIMIT = 128;
+                ///creates Acceptor sockets and binds it to port
+                ///Acceptor immediately starts listening on specified port
                 explicit Acceptor(unsigned short port) : Socket(){
                     Bind(port);
-                    if(listen(fd_, 128) == -1){
+                    if(listen(fd_, BACKLOG_LIMIT) == -1){
                         throw std::runtime_error("Acceptor: failed to listen on port " + std::to_string(port));
                     }
                 }
-                std::future<Socket> AsyncAccept() {
+                ///Asynchronously accepts connections
+                std::future<std::shared_ptr<Socket>> AsyncAccept() {
                     return std::async(std::launch::async, [this]() {
                         return Accept();
                     });
                 }
-                Socket Accept(){
+                ///Accepts connection and returns shared ptr to socket object for it
+                std::shared_ptr<Socket> Accept(){
                     sockaddr_in client_address;
                     socklen_t addr_len = sizeof(client_address);
                     std::memset(&client_address, 0, addr_len);
@@ -28,7 +34,7 @@ namespace s21{
                     if (client_fd == -1) {
                         throw std::runtime_error("Socket accept: Failed to accept connection");
                     }
-                    return static_cast<Socket>(Acceptor(client_fd));
+                    return CreateSocket(client_fd);
                 }
 
             };
