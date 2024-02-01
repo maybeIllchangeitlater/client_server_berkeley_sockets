623872d4cad686008e07c46f1b58a19858584ffd
monke <porozheg@gmail.com>
2024-02-01 19:30:15 +0300

    command line args to udp

--- src/udpip/client/client_main.cpp
+++ src/udpip/client/client_main.cpp
@@ -1,7 +1,13 @@
 #include "client.h"
 
 int main(int argc, char *argv[]) {
-  s21::udp::ip::Client client(argv[1], 8080);
-  client.Ping(std::chrono::seconds(std::atoi(argv[2])));
+  if (argc < 4) {
+    std::cout
+        << "please specify server port, client name and ping delay in seconds"
+        << std::endl;
+    exit(1);
+  }
+  s21::udp::ip::Client client(argv[1], argv[2]);
+  client.Ping(std::chrono::seconds(std::atoi(argv[3])));
   return 0;
 }
--- src/udpip/server/server_main.cpp
+++ src/udpip/server/server_main.cpp
@@ -1,6 +1,10 @@
 #include "server.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc) < 2 {
+      std::cout << "please enter port to run server on" << std::endl;
+      exit(1);
+    }
   s21::udp::ip::Server server(8080);
   std::cout << "server is running press q to exit" << std::endl;
   while (std::getchar() != 'q') {
